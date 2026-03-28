badb7286f7a8505067b9683c4b250ee6326ce260
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2026-03-28 22:56:17 +0200

    Initial Commit

---
+++ DarkroomLog/DarkroomLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkroomLogApp.swift
+//  DarkroomLog
+//
+//  Created by Vitalii Kovalyshyn on 28.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DarkroomLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
